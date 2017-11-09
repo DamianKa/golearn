ff40540f6e26512d5f553ddea95e0d3767c3d049
DamianKa <damcash@gmail.com>
2017-11-09 21:58:49 +0000

    about page added

--- golearn.go
+++ golearn.go
@@ -5,12 +5,18 @@ import ("fmt"
 
 func index_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Whoe, Go is neat!")
+}
+
+func about_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Expert web design ;)")
 
 }
 
 
 func main() {
 	http.HandleFunc("/", index_handler)
+	http.HandleFunc("/about/", about_handler)
+
 	http.ListenAndServe(":8000", nil)
 
 
