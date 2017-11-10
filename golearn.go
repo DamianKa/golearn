300f2cce260cebabc24a4351f42ff19fa0e325fc
DamianKa <damcash@gmail.com>
2017-11-10 20:02:55 +0000

    method

--- golearn.go
+++ golearn.go
@@ -2,11 +2,27 @@ package main
 
 import "fmt"
 
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
 type car struct {
 	gas_pedal uint16 //min 0 max 65535
 	brake_pedal uint16
 	steering_wheel int16 // -32k  -  32k
 	top_speed_kmh float64
+}
+
+func (c car) kmh() float64 {  //method but look like a function
+	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
+}
+
+func (c car) mph() float64 {  //method but look like a function
+	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax/kmh_multiple)
+}
+
+func (c *car) new_top_speed(newspeed float64){
+	c.top_speed_kmh
+
 }
 
 func main (){
@@ -15,6 +31,8 @@ func main (){
 			steering_wheel: 12561, 
 			top_speed_kmh: 225.0}
 
-
 	fmt.Println(a_car.gas_pedal)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
 }
+
